673215885b5c13d9d9fea8cf23698a07cb417d61
Illia Kozliuk <ikozluk160@gmail.com>
2025-07-02 14:29:59 +0300

    feat(server): ceanup & add swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('IoT API')
+    .setDescription('The IoT API description')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
