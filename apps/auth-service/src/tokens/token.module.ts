405a9e3183d14cf97fdc9380042608fba05a69af
Illia Kozliuk <ikozluk160@gmail.com>
2025-11-17 00:21:08 +0200

    feat(auth-service): add connect to redis

--- apps/auth-service/src/tokens/token.module.ts
+++ apps/auth-service/src/tokens/token.module.ts
@@ -4,7 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account, Token } from '@entities';
 import { JwtModule } from '@nestjs/jwt';
 import { options } from './config';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   providers: [TokenService],
@@ -12,6 +14,16 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule,
     TypeOrmModule.forFeature([Account, Token]),
     JwtModule.registerAsync(options()),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST'),
+        port: configService.get<number>('REDIS_PORT'),
+        ttl: 0,
+      }),
+    }),
   ],
   exports: [TokenService],
 })
